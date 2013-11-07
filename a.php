c2bdaa39d26b29d2ae07a79b30c4759d63b2e725
Delzon <delzon_only_for_you@hotmail.com>
2013-11-07 02:33:59 -0500

    a.php

--- a.php
+++ a.php
@@ -2,7 +2,7 @@
 $u = $_GET['u'];
 if ($u != "")
 {
-$urlimg = str_replace("s72-c", "s310-c", "$u");
+$urlimg = str_replace("s72-c", "s320-c", "$u");
 Header( "HTTP/1.1 301 Moved Permanently" );
 Header( "Location:". $urlimg . "" );		
 }
